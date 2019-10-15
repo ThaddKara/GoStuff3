7de6982fc2baaa51fbd906c769474369830f5f12
Zak <34078361+BoSkeet@users.noreply.github.com>
2019-10-15 04:39:30 +0000

    Initial commit

---
+++ gostuff/bot_v_bot.py
@@ -0,0 +1,37 @@
+from dlgo.agent import naive
+from dlgo import goboard_slow
+from dlgo import gotypes
+from dlgo import scoring
+from dlgo.utils import print_board, print_move
+import time
+import timeit
+
+
+def main():
+    while True:
+        print("The board size(5-19)")
+        board_size = int(input())
+        if 19 >= board_size >= 5:
+            break
+        else:
+            print("Wrong size,please input 5-19")
+    game = goboard_slow.GameState.new_game(board_size)
+    bots = {
+        gotypes.Player.black: naive.RandomBot(),
+        gotypes.Player.white: naive.RandomBot(),
+    }
+    start = timeit.default_timer()
+    while not game.is_over():
+        time.sleep(0.3)
+        print(chr(27) + "[2J")
+        print_board(game.board)
+        bot_move = bots[game.next_player].select_move(game)
+        print_move(game.next_player, bot_move)
+        game = game.apply_move(bot_move)
+    stop = timeit.default_timer()
+    print(scoring.compute_game_result(game))
+    print('Runtime:', stop - start, 'seconds')
+
+
+if __name__ == '__main__':
+    main()
---
+++ gostuff/human_v_bot.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+from dlgo.agent import naive
+from dlgo import goboard_slow as goboard
+from dlgo import gotypes
+from dlgo.utils import print_board, print_move, point_from_coords
+from six.moves import input
+from dlgo import scoring
+
+
+def main():
+    while True:
+        print("The board size(5-19)")
+        board_size = int(input())
+        if 19 >= board_size >= 5:
+            break
+        else:
+            print("Wrong size,please input 5-19")
+    game = goboard.GameState.new_game(board_size)
+    bot = naive.RandomBot()
+    while not game.is_over():
+        print(chr(27) + "[2J")
+        print_board(game.board)
+        if game.next_player == gotypes.Player.black:
+            human_move = input('-- ')
+            point = point_from_coords(human_move.strip())
+            move = goboard.Move.play(point)
+        else:
+            move = bot.select_move(game)
+        print_move(game.next_player, move)
+        game = game.apply_move(move)
+    print(scoring.compute_game_result(game))
+
+
+if __name__ == '__main__':
+    main()
